98bd49d43eeacd9850908908ff4fbee82b867cdb
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-04-08 11:05:09 +0530

    Add react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './components/about/About'
+import Resume from './components/resume/Resume'
+import Projects from './components/projects/Projects'
+import Github from './components/github/Github'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <About />,
+  },
+  {
+    path: "/resume",
+    element: <Resume />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/github",
+    element: <Github />,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
