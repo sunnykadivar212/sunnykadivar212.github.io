c6aeba37dcb3977dc5e0a3ed25aa1643fd37c3ac
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-04-09 11:21:55 +0530

    Add Resume Information

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,214 @@
-import React from 'react'
+import React from "react";
 
 function Resume() {
   return (
-    <div>Resume</div>
-  )
+    <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
+      <div className="flex w-full max-w-4xl flex-col gap-10 print:gap-6">
+        <div className="flex w-full max-w-4xl flex-col gap-36 print:gap-10">
+          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
+            <div
+              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
+              data-svelte-h="svelte-llc306"
+            >
+              <div className="print:col-start-2 lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">Experience</h2>
+              </div>
+            </div>
+            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
+              <div className="print:text-right lg:text-right">
+                <span className="text-black/60">Jan 2024 – Present </span>
+              </div>
+              <div className="print:col-span-3 lg:col-span-3">
+                <h2 className="inline font-bold">
+                  Full Stack devekoper intern
+                </h2>{" "}
+                <span className="[&_a]:underline">
+                  at{" "}
+                  <a
+                    href="https://greylabs.com/"
+                    target="_blank"
+                    rel="noreferrer nofollow"
+                  >
+                    Greycell labs
+                  </a>{" "}
+                  (Ahmedabad, India){" "}
+                </span>
+              </div>{" "}
+              <div className="print:text-right lg:text-right">
+                <span className="text-sm text-black/60">Full-time </span>
+              </div>
+              <div className="print:col-span-3 lg:col-span-3">
+                <p>
+                  actively helping in feature and performance improvement
+                  projects such as:{" "}
+                </p>
+                <ul className="mx-5 my-2 flex list-disc flex-col gap-1 text-sm">
+                  <li className="[&_a]:underline">
+                    <a
+                      href="https://www.bidclips.com/"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      bidclips.com
+                    </a>
+                    : BidClips is a software platform designed for home service
+                    businesses. It helps streamline tasks like lead management,
+                    creating estimates, communicating with customers, scheduling
+                    jobs, and processing online payments, all in one place.{" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
+            <div
+              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
+              data-svelte-h="svelte-1qclnao"
+            >
+              <div className="print:col-start-2 lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600 ">Education</h2>
+              </div>
+            </div>
+            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
+              <div className="print:text-right lg:text-right">
+                <span className="text-black/60">Jan 2022 – Jan 2024 </span>
+              </div>
+              <div className="print:col-span-3 lg:col-span-3">
+                <h2 className="inline font-bold">
+                  Master of Computer Applications
+                </h2>{" "}
+                <span>at Gujarat University </span>
+              </div>
+              <div
+                className="print:text-right lg:text-right"
+                data-svelte-h="svelte-yw87rh"
+              >
+                <span className="text-sm text-black/60" />
+              </div>
+              <div className="print:col-span-3 lg:col-span-3">
+                <p>Ahmedabad, India</p>
+              </div>
+            </div>
+            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
+              <div className="print:text-right lg:text-right">
+                <span className="text-black/60">Jan 2019 – Jan 2022 </span>
+              </div>
+              <div className="print:col-span-3 lg:col-span-3">
+                <h2 className="inline font-bold">
+                  Bachelor of Computer Applications
+                </h2>{" "}
+                <span>at GLS University </span>
+              </div>
+              <div
+                className="print:text-right lg:text-right"
+                data-svelte-h="svelte-yw87rh"
+              >
+                <span className="text-sm text-black/60" />
+              </div>
+              <div className="print:col-span-3 lg:col-span-3">
+                <p>Ahmedabad, India</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
+            <div
+              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
+              data-svelte-h="svelte-1prmtjf"
+            >
+              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">
+                  Skills and Tech Stack
+                </h2>
+              </div>
+            </div>
+            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
+              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                <ul className="mx-5 my-2 flex list-disc flex-col gap-1 text-sm">
+                  <li>HTML5, CSS, JavaScript, Java, Python</li>
+                  <li>JavaScript libraries including React, Redux</li>
+                  <li>Git, GitHub</li>
+                  <li>
+                    Currently I am improving my skills by building some projects
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
+            <div
+              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
+              data-svelte-h="svelte-vldwmu"
+            >
+              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">
+                  Contact Info
+                </h2>
+              </div>
+            </div>
+            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
+              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
+                <ul className="mx-5 my-2 flex list-disc flex-col gap-1 text-sm">
+                  <li className="[&_a]:underline">
+                    Email:{" "}
+                    <a
+                      href="mailto:vaghelaashish111@gmail.com"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      vaghelaashish111@gmail.com
+                    </a>{" "}
+                  </li>
+                  <li className="[&_a]:underline">
+                    Linkedin:{" "}
+                    <a
+                      href="https://linkedin.com/in/ashish-codejourney/"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      linkedin.com/in/ashish-codejourney
+                    </a>{" "}
+                  </li>
+                  <li className="[&_a]:underline">
+                    Github:{" "}
+                    <a
+                      href="https://github.com/Ashish-CodeJourney"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      github.com/Ashish-CodeJourney
+                    </a>{" "}
+                  </li>
+                  <li className="[&_a]:underline">
+                    Twitter:{" "}
+                    <a
+                      href="https://twitter.com/CLI_Craftsman"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      @CLI_Craftsman
+                    </a>{" "}
+                  </li>
+                  <li className="[&_a]:underline">
+                    Telegram:{" "}
+                    <a
+                      href="https://t.me/heyyy_ashish"
+                      target="_blank"
+                      rel="noreferrer nofollow"
+                    >
+                      @heyyy_ashish
+                    </a>{" "}
+                  </li>
+                  <li className="[&_a]:underline">
+                    Location: Ahmedabad, India (open to relocation){" "}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
