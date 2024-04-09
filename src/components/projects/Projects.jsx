80eb92b41204726f802e4d112401246f7b704e99
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-04-09 12:22:19 +0530

    Add projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,7 +2,69 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className="flex flex-col gap-16 lg:gap-6">
+  <div className="my-4 grid gap-2 md:grid-cols-2 lg:gap-10">
+    <a
+      href="https://www.kmaster.ashishcodejourney.tech/"
+      className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+    >
+      <img
+        src="/src/assets/projects/kmaster.jpg"
+        alt="daisyUI"
+        width={350}
+        height={350}
+        className="rounded-lg transition-all duration-300"
+        style={{ viewTransitionName: "img-kmaster" }}
+      />{" "}
+      <div
+        className="text-center text-sm"
+        style={{ viewTransitionName: "title-kmaster" }}
+      >
+        Keyboard Master
+      </div>{" "}
+    </a>
+    <a
+      href="https://github.com/Ashish-CodeJourney/CYBERTRACE"
+      className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+    >
+      <img
+        src="/src/assets/projects/cybertrace.jpg"
+        alt="project1"
+        width={350}
+        height={350}
+        className="rounded-lg transition-all duration-300"
+        style={{ viewTransitionName: "img-cybertrace" }}
+      />{" "}
+      <div
+        className="text-center text-sm"
+        style={{ viewTransitionName: "title-cybertrace" }}
+      >
+        CyberTrace
+      </div>{" "}
+    </a>
+    <a
+      href="https://github.com/Ashish-CodeJourney/Library_management"
+      className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+    >
+      <img
+        src="/src/assets/projects/library.jpg"
+        alt="Atbox.io"
+        width={350}
+        height={350}
+        className="rounded-lg transition-all duration-300"
+        style={{ viewTransitionName: "img-library" }}
+      />{" "}
+      <div
+        className="text-center text-sm"
+        style={{ viewTransitionName: "title-library" }}
+      >
+        Library Management system
+      </div>{" "}
+    </a>
+
+  </div>
+</div>
+
   )
 }
 
