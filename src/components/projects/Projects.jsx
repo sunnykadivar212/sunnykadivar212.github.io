c6a443047274a0765406067aa602f4809643e727
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-04-09 12:33:05 +0530

    Add project template

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,71 +1,89 @@
-import React from 'react'
+import React from "react";
 
 function Projects() {
   return (
     <div className="flex flex-col gap-16 lg:gap-6">
-  <div className="my-4 grid gap-2 md:grid-cols-2 lg:gap-10">
-    <a
-      href="https://www.kmaster.ashishcodejourney.tech/"
-      className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    >
-      <img
-        src="../public/projects/kmaster.jpg"
-        alt="daisyUI"
-        width={350}
-        height={350}
-        className="rounded-lg transition-all duration-300"
-        style={{ viewTransitionName: "img-kmaster" }}
-      />{" "}
-      <div
-        className="text-center text-sm"
-        style={{ viewTransitionName: "title-kmaster" }}
-      >
-        Keyboard Master
-      </div>{" "}
-    </a>
-    <a
-      href="https://github.com/Ashish-CodeJourney/CYBERTRACE"
-      className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    >
-      <img
-        src="../public/projects/cybertrace.jpg"
-        alt="project1"
-        width={350}
-        height={350}
-        className="rounded-lg transition-all duration-300"
-        style={{ viewTransitionName: "img-cybertrace" }}
-      />{" "}
-      <div
-        className="text-center text-sm"
-        style={{ viewTransitionName: "title-cybertrace" }}
-      >
-        CyberTrace
-      </div>{" "}
-    </a>
-    <a
-      href="https://github.com/Ashish-CodeJourney/Library_management"
-      className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    >
-      <img
-        src="../public/projects/library.jpg"
-        alt="Library Management system"
-        width={350}
-        height={350}
-        className="rounded-lg transition-all duration-300"
-        style={{ viewTransitionName: "img-library" }}
-      />{" "}
-      <div
-        className="text-center text-sm"
-        style={{ viewTransitionName: "title-library" }}
-      >
-        Library Management system
-      </div>{" "}
-    </a>
+      <div className="my-4 grid gap-2 md:grid-cols-2 lg:gap-10">
+        <a
+          href="https://www.ashishcodejourney.tech/"
+          className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+        >
+          <img
+            src="#"
+            alt="project1"
+            width={350}
+            height={350}
+            className="rounded-lg transition-all duration-300"
+            style={{ viewTransitionName: "img-project1" }}
+          />{" "}
+          <div
+            className="text-center text-sm"
+            style={{ viewTransitionName: "title-project1" }}
+          >
+            project1
+          </div>{" "}
+        </a>
+        <a
+          href="https://www.ashishcodejourney.tech/"
+          className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+        >
+          <img
+            src="#"
+            alt="project2"
+            width={350}
+            height={350}
+            className="rounded-lg transition-all duration-300"
+            style={{ viewTransitionName: "img-project2" }}
+          />{" "}
+          <div
+            className="text-center text-sm"
+            style={{ viewTransitionName: "title-project2" }}
+          >
+            project2
+          </div>{" "}
+        </a>
+        <a
+          href="https://www.ashishcodejourney.tech/"
+          className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+        >
+          <img
+            src="#"
+            alt="project3"
+            width={350}
+            height={350}
+            className="rounded-lg transition-all duration-300"
+            style={{ viewTransitionName: "img-project3" }}
+          />{" "}
+          <div
+            className="text-center text-sm"
+            style={{ viewTransitionName: "title-project3" }}
+          >
+            project3
+          </div>{" "}
+        </a>
 
-  </div>
-</div>
-
-  )
+        <a
+          href="https://www.ashishcodejourney.tech/"
+          className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
+        >
+          <img
+            src="#"
+            alt="project4"
+            width={350}
+            height={350}
+            className="rounded-lg transition-all duration-300"
+            style={{ viewTransitionName: "img-project4" }}
+          />{" "}
+          <div
+            className="text-center text-sm"
+            style={{ viewTransitionName: "title-project4" }}
+          >
+            project4
+          </div>{" "}
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
