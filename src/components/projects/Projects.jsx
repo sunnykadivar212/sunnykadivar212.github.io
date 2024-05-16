fd1cd15a596088bcc5d6d7732f55388ed4954700
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-05-16 15:00:44 +0530

    Project page updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Lottie from 'react-lottie';
-import animationData from "../../assets/under-construction.json"
+import animationData from "../../assets/under-construction.json";
+import data from '../../assets/data.json';
 
 function Projects() {
   const defaultOptions = {
@@ -13,102 +13,40 @@ function Projects() {
   };
 
   return (
-    // uncomment this and remove other thing of animation (lottie) when add projects
-    // <div className="flex flex-col gap-16 lg:gap-6">
-    //   <div className="my-4 grid gap-2 md:grid-cols-2 lg:gap-10">
-    //     <a
-    //       href="https://www.ashishcodejourney.tech/"
-    //       className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    //     >
-    //       <img
-    //         src="#"
-    //         alt="project1"
-    //         width={350}
-    //         height={350}
-    //         className="rounded-lg transition-all duration-300"
-    //         style={{ viewTransitionName: "img-project1" }}
-    //       />{" "}
-    //       <div
-    //         className="text-center text-sm"
-    //         style={{ viewTransitionName: "title-project1" }}
-    //       >
-    //         project1
-    //       </div>{" "}
-    //     </a>
-    //     <a
-    //       href="https://www.ashishcodejourney.tech/"
-    //       className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    //     >
-    //       <img
-    //         src="#"
-    //         alt="project2"
-    //         width={350}
-    //         height={350}
-    //         className="rounded-lg transition-all duration-300"
-    //         style={{ viewTransitionName: "img-project2" }}
-    //       />{" "}
-    //       <div
-    //         className="text-center text-sm"
-    //         style={{ viewTransitionName: "title-project2" }}
-    //       >
-    //         project2
-    //       </div>{" "}
-    //     </a>
-    //     <a
-    //       href="https://www.ashishcodejourney.tech/"
-    //       className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    //     >
-    //       <img
-    //         src="#"
-    //         alt="project3"
-    //         width={350}
-    //         height={350}
-    //         className="rounded-lg transition-all duration-300"
-    //         style={{ viewTransitionName: "img-project3" }}
-    //       />{" "}
-    //       <div
-    //         className="text-center text-sm"
-    //         style={{ viewTransitionName: "title-project3" }}
-    //       >
-    //         project3
-    //       </div>{" "}
-    //     </a>
-
-    //     <a
-    //       href="https://www.ashishcodejourney.tech/"
-    //       className="flex flex-col bg-slate-50 items-center gap-4 rounded-2xl py-20 text-slate-700 transition-all duration-700 hover:bg-slate-200"
-    //     >
-    //       <img
-    //         src="#"
-    //         alt="project4"
-    //         width={350}
-    //         height={350}
-    //         className="rounded-lg transition-all duration-300"
-    //         style={{ viewTransitionName: "img-project4" }}
-    //       />{" "}
-    //       <div
-    //         className="text-center text-sm"
-    //         style={{ viewTransitionName: "title-project4" }}
-    //       >
-    //         project4
-    //       </div>{" "}
-    //     </a>
-    //   </div>
-    // </div>
-
-    <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
-      <div className="flex w-full max-w-4xl flex-col gap-10 print:gap-6">
-        <div className="flex flex-col items-center">
-          <Lottie options={defaultOptions} height={300} width={300} />
-          <p className="text-left leading-8 mt-8">
-            <span className="text-lg">
-              <strong>This page is under construction.</strong>
-            </span>
-            <br />
-            <span className="text-lg">
-              Please check back later for updates.
-            </span>
-          </p>
+    <div className="flex flex-col gap-16 lg:gap-6">
+      <div className="max-w-screen-lg mx-auto bg-white rounded-lg shadow-md p-6">
+        <div className="flex flex-col items-center gap-4 rounded-2xl py-8 lg:py-12 px-6 lg:px-10 text-slate-700 transition-all duration-700 hover:bg-slate-200">
+          <img
+            src={data.project1ImageUrl}
+            alt="project1"
+            width={350}
+            height={200}
+            className="rounded-lg shadow-lg transition-all duration-300"
+          />
+          <div className="text-center text-lg font-semibold mt-4">{data.project1Name}</div>
+          <div className="text-center text-m text-black-700 mt-2 ">
+            {data.project1Description}
+          </div>
+          
+          <a href={data.project1GithubUrl} className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+          <a href={data.project1LiveUrl} className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" target="_blank" rel="noopener noreferrer">Live</a>
+          
+        </div>
+      </div>
+      <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
+        <div className="flex w-full max-w-4xl flex-col gap-10 print:gap-6">
+          <div className="flex flex-col items-center">
+            <Lottie options={defaultOptions} height={300} width={300} />
+            <p className="text-left leading-8 mt-8">
+              <span className="text-lg">
+                <strong>Other projects are under construction.</strong>
+              </span>
+              <br />
+              <span className="text-lg">
+                Please check back later for updates.
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
