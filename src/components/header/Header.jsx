770545bc34305ee877dfb88a3363afe8cb42a440
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-05-20 10:59:35 +0530

    aligned theme button in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -24,68 +24,64 @@ function Header() {
   return (
     <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
       <div className="flex w-full max-w-4xl flex-col gap-36 print:gap-10">
-        <div className="flex gap-10 print:hidden">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "border-blue-600" : "border-b-transparent"
-              } text-sm py-1 border-b-2`
-            }
-          >
-            About
-          </NavLink>
-          <NavLink
-            to="/resume/"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "border-blue-600" : "border-b-transparent"
-              } text-sm py-1 border-b-2`
-            }
-          >
-            Resume
-          </NavLink>
-          <NavLink
-            to="/projects/"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "border-blue-600" : "border-b-transparent"
-              } text-sm py-1 border-b-2`
-            }
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/github/"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 ${
-                isActive ? "border-blue-600" : "border-b-transparent"
-              } text-sm py-1 border-b-2`
-            }
-          >
-            Github
-          </NavLink>
-
-          <label className="swap swap-rotate">
-            {/* this hidden checkbox controls the state */}
+        <div className="flex items-center justify-between w-full print:hidden">
+          <div className="flex gap-4">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "border-blue-600" : "border-b-transparent"
+                } text-sm py-1 border-b-2`
+              }
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/resume/"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "border-blue-600" : "border-b-transparent"
+                } text-sm py-1 border-b-2`
+              }
+            >
+              Resume
+            </NavLink>
+            <NavLink
+              to="/projects/"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "border-blue-600" : "border-b-transparent"
+                } text-sm py-1 border-b-2`
+              }
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="/github/"
+              className={({ isActive }) =>
+                `block py-2 pr-4 pl-3 duration-200 ${
+                  isActive ? "border-blue-600" : "border-b-transparent"
+                } text-sm py-1 border-b-2`
+              }
+            >
+              Github
+            </NavLink>
+          </div>
+          <label className="swap swap-rotate ml-4">
             <input
               type="checkbox"
               onChange={handleToggle}
               checked={theme === "light" ? false : true}
             />
-
-            {/* sun icon */}
             <svg
-              className="swap-on fill-current w-10 h-10"
+              className="swap-on fill-current w-8 h-8"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
             >
               <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
             </svg>
-
-            {/* moon icon */}
             <svg
-              className="swap-off fill-current w-10 h-10"
+              className="swap-off fill-current w-8 h-8"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
             >
