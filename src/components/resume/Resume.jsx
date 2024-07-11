48faeaf9ce984b2d4d68e35147dca258d0479dc5
vaghelaashish111 <vaghelaashish111@gmail.com>
2024-07-11 05:12:18 +0000

    Open source contributions added

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -3,197 +3,128 @@ import data from "../../assets/data.json";
 function Resume() {
   return (
     <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
-      <div className="flex w-full max-w-4xl flex-col gap-10 print:gap-6">
-        <div className="flex w-full max-w-4xl flex-col gap-36 print:gap-10">
-          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
-            <div
-              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
-              data-svelte-h="svelte-llc306"
-            >
-              <div className="print:col-start-2 lg:col-span-3 lg:col-start-2">
+      <div className="flex w-full max-w-4xl flex-col gap-10">
+        <div className="flex w-full max-w-4xl flex-col gap-36">
+          {/* Experience */}
+          <div className="flex flex-col gap-16">
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
                 <h2 className="text-2xl font-bold text-blue-600">Experience</h2>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-              <div className="print:text-right lg:text-right">
-                <span className="inline">
-                  {data.workingInCurrentCompanySince}{" "}
-                </span>
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:text-right">
+                <span>{data.workingInCurrentCompanySince}</span>
               </div>
-              <div className="print:col-span-3 lg:col-span-3">
+              <div className="lg:col-span-3">
                 <h2 className="inline font-bold">{data.currentPosition}</h2>{" "}
-                <span className="[&_a]:underline">
-                  at{" "}
-                  <a
-                    href={data.companySite}
-                    target="_blank"
-                    rel="noreferrer nofollow"
-                  >
-                    {data.company}
-                  </a>{" "}
-                  ({data.companyLocation}){" "}
+                <span>
+                  at <a href={data.companySite}>{data.company}</a> ({data.companyLocation})
                 </span>
-              </div>{" "}
-              <div className="print:text-right lg:text-right">
-                <span className="inline">{data.workingShift}</span>
               </div>
-              <div className="print:col-span-3 lg:col-span-3">
-                <p>
-                  actively helping in feature and performance improvement
-                  projects such as:{" "}
-                </p>
-                <ul className="mx-5 my-2 flex list-disc flex-col gap-1 text-sm">
-                  <li className="[&_a]:underline">
-                    <a
-                      href={data.workingProjectSite}
-                      target="_blank"
-                      rel="noreferrer nofollow"
-                    >
-                      {data.workingOn}
-                    </a>
-                    : {data.workingProjectDescription}.{" "}
+              <div className="lg:text-right">
+                <span>{data.workingShift}</span>
+              </div>
+              <div className="lg:col-span-3">
+                <p>actively helping in feature and performance improvement projects such as:</p>
+                <ul className="mx-5 my-2 list-disc">
+                  <li>
+                    <a href={data.workingProjectSite}>{data.workingOn}</a>: {data.workingProjectDescription}
                   </li>
                 </ul>
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
-            <div
-              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
-              data-svelte-h="svelte-1qclnao"
-            >
-              <div className="print:col-start-2 lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600 ">Education</h2>
+
+          {/* Open Source Contributions */}
+          <div className="flex flex-col gap-16">
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">Open Source Contributions</h2>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-              <div className="print:text-right lg:text-right">
-                <span className="inline">{data.mastersCollegeTime} </span>
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:text-right">
+                <span>{data.openSourceContributionSince}</span>
               </div>
-              <div className="print:col-span-3 lg:col-span-3">
-                <h2 className="inline font-bold">{data.mastersDegree}</h2>{" "}
-                <span>at {data.mastersUniversity} </span>
+              <div className="lg:col-span-3">
+                <h2 className="inline font-bold">{data.openSourceContributionsTitle}</h2>
+                <ul className="mx-5 my-2 list-disc">
+                  <li>{data.openSourceContributionDetails1}</li>
+                  <li>{data.openSourceContributionsDetails2}</li>
+                  <li>{data.openSourceContributionsDetails3}</li>
+                  <li>
+                    <a href={data.openSourceContributionsUrl}>
+                      {data.openSourceContributionsUrlText}
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <div
-                className="print:text-right lg:text-right"
-                data-svelte-h="svelte-yw87rh"
-              >
-                <span className="inline" />
+            </div>
+          </div>
+
+          {/* Education */}
+          <div className="flex flex-col gap-16">
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">Education</h2>
               </div>
-              <div className="print:col-span-3 lg:col-span-3">
+            </div>
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:text-right">
+                <span>{data.mastersCollegeTime}</span>
+              </div>
+              <div className="lg:col-span-3">
+                <h2 className="inline font-bold">{data.mastersDegree}</h2> <span>at {data.mastersUniversity}</span>
                 <p>{data.mastersUniversityLocation}</p>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-              <div className="print:text-right lg:text-right">
-                <span className="inline">{data.bacheloursCollegeTime}</span>
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:text-right">
+                <span>{data.bacheloursCollegeTime}</span>
               </div>
-              <div className="print:col-span-3 lg:col-span-3">
-                <h2 className="inline font-bold">{data.bacheloursDegree}</h2>{" "}
-                <span>at {data.bacheloursUniversity} </span>
-              </div>
-              <div
-                className="print:text-right lg:text-right"
-                data-svelte-h="svelte-yw87rh"
-              >
-                <span className="inline" />
-              </div>
-              <div className="print:col-span-3 lg:col-span-3">
+              <div className="lg:col-span-3">
+                <h2 className="inline font-bold">{data.bacheloursDegree}</h2> <span>at {data.bacheloursUniversity}</span>
                 <p>{data.bacheloursUniversityLocation}</p>
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
-            <div
-              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
-              data-svelte-h="svelte-1prmtjf"
-            >
-              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">
-                  Skills and Tech Stack
-                </h2>
+
+          {/* Skills and Tech Stack */}
+          <div className="flex flex-col gap-16">
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">Skills and Tech Stack</h2>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
-                <ul className="mx-5 my-2 flex list-disc flex-col gap-1 text-sm">
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
+                <ul className="mx-5 my-2 list-disc">
                   <li>{data.knownSkills}</li>
                   <li>{data.knownLibrariesAndFrameWorks}</li>
                   <li>{data.knownTools}</li>
-                  <li>
-                    Currently I am improving my skills by building some projects
-                  </li>
+                  <li>Currently I am improving my skills by building some projects</li>
                 </ul>
               </div>
             </div>
           </div>
-          <div className="flex flex-col gap-16 print:gap-6 lg:gap-6">
-            <div
-              className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4"
-              data-svelte-h="svelte-vldwmu"
-            >
-              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">
-                  Contact Info
-                </h2>
+
+          {/* Contact Info */}
+          <div className="flex flex-col gap-16">
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
+                <h2 className="text-2xl font-bold text-blue-600">Contact Info</h2>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 print:grid-cols-4 lg:grid-cols-4">
-              <div className="print:col-span-3 print:col-start-2 lg:col-span-3 lg:col-start-2">
-                <ul className="mx-5 my-2 flex list-disc flex-col gap-1 text-sm">
-                  <li className="[&_a]:underline">
-                    Email:{" "}
-                    <a
-                      href={data.mailto}
-                      target="_blank"
-                      rel="noreferrer nofollow"
-                    >
-                      {data.email}
-                    </a>{" "}
-                  </li>
-                  <li className="[&_a]:underline">
-                    Linkedin:{" "}
-                    <a
-                      href={data.linkedinUrl}
-                      target="_blank"
-                      rel="noreferrer nofollow"
-                    >
-                      @{data.linkedinUserName}
-                    </a>{" "}
-                  </li>
-                  <li className="[&_a]:underline">
-                    Github:{" "}
-                    <a
-                      href="https://github.com/Ashish-CodeJourney"
-                      target="_blank"
-                      rel="noreferrer nofollow"
-                    >
-                      @{data.githubUserName}
-                    </a>{" "}
-                  </li>
-                  <li className="[&_a]:underline">
-                    X:{" "}
-                    <a
-                      href={data.XUrl}
-                      target="_blank"
-                      rel="noreferrer nofollow"
-                    >
-                      @{data.XUserName}
-                    </a>{" "}
-                  </li>
-                  <li className="[&_a]:underline">
-                    Telegram:{" "}
-                    <a
-                      href={data.telegramUrl}
-                      target="_blank"
-                      rel="noreferrer nofollow"
-                    >
-                      @{data.telegramUserName}
-                    </a>{" "}
-                  </li>
-                  <li className="[&_a]:underline">
-                    Location: {data.city} (open to relocation){" "}
-                  </li>
+            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+              <div className="lg:col-span-3 lg:col-start-2">
+                <ul className="mx-5 my-2 list-disc">
+                  <li>Email: <a href={data.mailto}>{data.email}</a></li>
+                  <li>Linkedin: <a href={data.linkedinUrl}>@{data.linkedinUserName}</a></li>
+                  <li>Github: <a href="https://github.com/Ashish-CodeJourney">@{data.githubUserName}</a></li>
+                  <li>X: <a href={data.XUrl}>@{data.XUserName}</a></li>
+                  <li>Telegram: <a href={data.telegramUrl}>@{data.telegramUserName}</a></li>
+                  <li>Location: {data.city} (open to relocation)</li>
                 </ul>
               </div>
             </div>
