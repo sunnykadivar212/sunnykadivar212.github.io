67e803e2e91ca4faf1f08071aeb20c9655d489b0
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2024-12-03 17:33:08 +0530

    refactor: Improved some styling and data

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,158 +2,175 @@ import data from "../../assets/data.json";
 
 function Resume() {
   return (
-    <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
-      <div className="flex w-full max-w-4xl flex-col gap-10">
-        <div className="flex w-full max-w-4xl flex-col gap-36">
-          {/* Experience */}
-          <div className="flex flex-col gap-16">
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">Experience</h2>
+      <div className="mb-24 mt-6 flex flex-col items-center justify-center px-6">
+        <div className="flex w-full max-w-4xl flex-col gap-10">
+          <div className="flex w-full max-w-4xl flex-col gap-36">
+            {/* Experience */}
+            <div className="flex flex-col gap-16">
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <h2 className="text-2xl font-bold text-blue-600">Experience</h2>
+                </div>
               </div>
-            </div>
 
-            {/* Current Job */}
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:text-right">
-                <span>Sep 2024 – Present</span>
-              </div>
-              <div className="lg:col-span-3">
-                <h2 className="inline font-bold">Software Craftsperson</h2>{" "}
-                <span>
-                  at <a href="https://nelkinda.com">Nelkinda Software Craft</a> (Remote)
+              {/* Current Job */}
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:text-right">
+                  <span>{data.currentJobStartDate} – Present</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <h2 className="inline font-bold">{data.currentPosition}</h2>{" "}
+                  <span>
+                  at <a href={data.currentCompanySite}>{data.currentCompany}</a> ({data.currentCompanyLocation})
                 </span>
+                </div>
+                <div className="lg:text-right">
+                  <span>{data.currentJobType}</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <p>{data.currentJobDescription}</p>
+                </div>
               </div>
-              <div className="lg:text-right">
-                <span>Full-time</span>
-              </div>
-              <div className="lg:col-span-3">
-                <p>Actively involved in writing clean, maintainable code, following XP practices, TDD, and software craftsmanship principles.</p>
-              </div>
-            </div>
 
-            {/* Old Job */}
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:text-right">
-                <span>{data.workingInCurrentCompanySince} </span>
-              </div>
-              <div className="lg:col-span-3">
-                <h2 className="inline font-bold">Full Stack Developer Intern</h2>{" "}
-                <span>
-                  at <a href={data.companySite}>{data.company}</a> ({data.companyLocation})
+              {/* Previous Job */}
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:text-right">
+                  <span>{data.previousCompanyStartDate} – {data.previousCompanyEndDate}</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <h2 className="inline font-bold">{data.previousJobTitle}</h2>{" "}
+                  <span>
+                  at <a href={data.previousCompanySite}>{data.previousCompany}</a> ({data.previousCompanyLocation})
                 </span>
-              </div>
-              <div className="lg:text-right">
-                <span>{data.workingShift}</span>
-              </div>
-              <div className="lg:col-span-3">
-                <p>Worked on various feature and performance improvement projects such as:</p>
-                <ul className="mx-5 my-2 list-disc">
-                  <li>
-                    <a href={data.workingProjectSite}>{data.workingOn}</a>: {data.workingProjectDescription}
-                  </li>
-                </ul>
+                </div>
+                <div className="lg:text-right">
+                  <span>{data.previousJobType}</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <p>{data.previousJobDescription}</p>
+                  <ul className="mx-5 my-2 list-disc">
+                    {data.previousProjects.map((project, index) => (
+                        <li key={index}>
+                          <a href={project.site}>{project.name}</a>: {project.description}
+                        </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Open Source Contributions */}
-          <div className="flex flex-col gap-16">
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">Open Source Contributions</h2>
+            {/* Open Source Contributions */}
+            <div className="flex flex-col gap-16">
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <h2 className="text-2xl font-bold text-blue-600">Open Source Contributions</h2>
+                </div>
+              </div>
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:text-right">
+                  <span>{data.openSourceContributionSince}</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <h2 className="inline font-bold">{data.openSourceContributionsTitle}</h2>
+                  <ul className="mx-5 my-2 list-disc">
+                    <li>{data.openSourceContributionDetails1}</li>
+                    <li>{data.openSourceContributionsDetails2}</li>
+                    <li>{data.openSourceContributionsDetails3}</li>
+                    <li>
+                      <a href={data.openSourceContributionsUrl}>
+                        {data.openSourceContributionsUrlText}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:text-right">
-                <span>{data.openSourceContributionSince}</span>
-              </div>
-              <div className="lg:col-span-3">
-                <h2 className="inline font-bold">{data.openSourceContributionsTitle}</h2>
-                <ul className="mx-5 my-2 list-disc">
-                  <li>{data.openSourceContributionDetails1}</li>
-                  <li>{data.openSourceContributionsDetails2}</li>
-                  <li>{data.openSourceContributionsDetails3}</li>
-                  <li>
-                    <a href={data.openSourceContributionsUrl}>
-                      {data.openSourceContributionsUrlText}
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
 
-          {/* Education */}
-          <div className="flex flex-col gap-16">
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">Education</h2>
+            {/* Education */}
+            <div className="flex flex-col gap-16">
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <h2 className="text-2xl font-bold text-blue-600">Education</h2>
+                </div>
+              </div>
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:text-right">
+                  <span>{data.mastersCollegeTime}</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <h2 className="inline font-bold">{data.mastersDegree}</h2>{" "}
+                  <span>at {data.mastersUniversity}</span>
+                  <p>{data.mastersUniversityLocation}</p>
+                </div>
+              </div>
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:text-right">
+                  <span>{data.bachelorsCollegeTime}</span>
+                </div>
+                <div className="lg:col-span-3">
+                  <h2 className="inline font-bold">{data.bachelorsDegree}</h2>{" "}
+                  <span>at {data.bachelorsUniversity}</span>
+                  <p>{data.bachelorsUniversityLocation}</p>
+                </div>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:text-right">
-                <span>{data.mastersCollegeTime}</span>
-              </div>
-              <div className="lg:col-span-3">
-                <h2 className="inline font-bold">{data.mastersDegree}</h2> <span>at {data.mastersUniversity}</span>
-                <p>{data.mastersUniversityLocation}</p>
-              </div>
-            </div>
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:text-right">
-                <span>{data.bacheloursCollegeTime}</span>
-              </div>
-              <div className="lg:col-span-3">
-                <h2 className="inline font-bold">{data.bacheloursDegree}</h2> <span>at {data.bacheloursUniversity}</span>
-                <p>{data.bacheloursUniversityLocation}</p>
-              </div>
-            </div>
-          </div>
 
-          {/* Skills and Tech Stack */}
-          <div className="flex flex-col gap-16">
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">Skills and Tech Stack</h2>
+            {/* Skills and Tech Stack */}
+            <div className="flex flex-col gap-16">
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <h2 className="text-2xl font-bold text-blue-600">Skills and Tech Stack</h2>
+                </div>
+              </div>
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <ul className="mx-5 my-2 list-disc">
+                    <li>{data.knownSkills}</li>
+                    <li>{data.knownLibrariesAndFrameWorks}</li>
+                    <li>{data.knownTools}</li>
+                    <li>Currently improving skills by building projects</li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <ul className="mx-5 my-2 list-disc">
-                  <li>{data.knownSkills}</li>
-                  <li>{data.knownLibrariesAndFrameWorks}</li>
-                  <li>{data.knownTools}</li>
-                  <li>Currently I am improving my skills by building some projects</li>
-                </ul>
-              </div>
-            </div>
-          </div>
 
-          {/* Contact Info */}
-          <div className="flex flex-col gap-16">
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <h2 className="text-2xl font-bold text-blue-600">Contact Info</h2>
+            {/* Contact Info */}
+            <div className="flex flex-col gap-16">
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <h2 className="text-2xl font-bold text-blue-600">Contact Info</h2>
+                </div>
               </div>
-            </div>
-            <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
-              <div className="lg:col-span-3 lg:col-start-2">
-                <ul className="mx-5 my-2 list-disc">
-                  <li>Email: <a href={data.mailto}>{data.email}</a></li>
-                  <li>Linkedin: <a href={data.linkedinUrl}>@{data.linkedinUserName}</a></li>
-                  <li>Github: <a href={data.githubUrl}>@{data.githubUserName}</a></li>
-                  <li>X: <a href={data.XUrl}>@{data.XUserName}</a></li>
-                  <li>Dev.to: <a href={data.devToUrl}>@{data.XUserName}</a></li>
-                  <li>Telegram: <a href={data.telegramUrl}>@{data.devToUserName}</a></li>
-                  <li>Location: {data.city} (open to relocation)</li>
-                </ul>
+              <div className="grid items-baseline gap-x-10 lg:grid-cols-4">
+                <div className="lg:col-span-3 lg:col-start-2">
+                  <ul className="mx-5 my-2 list-disc">
+                    <li>
+                      Email: <a href={data.mailto}>{data.email}</a>
+                    </li>
+                    <li>
+                      LinkedIn:{" "}
+                      <a href={data.linkedinUrl}>@{data.linkedinUserName}</a>
+                    </li>
+                    <li>
+                      GitHub: <a href={data.githubUrl}>@{data.githubUserName}</a>
+                    </li>
+                    <li>
+                      X: <a href={data.XUrl}>@{data.XUserName}</a>
+                    </li>
+                    <li>
+                      Dev.to: <a href={data.devToUrl}>@{data.devToUserName}</a>
+                    </li>
+                    <li>
+                      Telegram: <a href={data.telegramUrl}>@{data.telegramUserName}</a>
+                    </li>
+                    <li>Location: {data.city} (open to relocation)</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
