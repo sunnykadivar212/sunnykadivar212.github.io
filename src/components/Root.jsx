915d4af4fca8fa69b43f0613fba3b9dbbea32c1d
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2025-02-20 09:56:20 +0530

    refactor: Remove unused routes and navigation to notfound page when trying to access invalid url path

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './header/Header';
 import About from './about/About';
 import Resume from './resume/Resume';
 import Projects from './projects/Projects';
 import Certifications from './certification/Certification';
 import Github from './github/Github';
+import NotFound from './notfound/NotFound';
 
 function Root() {
-  const [view, setView] = useState('about');
+    const location = useLocation();
+    const [view, setView] = useState('about');
+
+    useEffect(() => {
+        const path = location.pathname.substring(1);
+        if (path === '') {
+            setView('about');
+        } else if (['about', 'resume', 'projects', 'certification', 'github'].includes(path)) {
+            setView(path);
+        } else {
+            setView('notfound');
+        }
+    }, [location]);
 
   const renderView = () => {
     switch (view) {
@@ -21,6 +35,8 @@ function Root() {
         return <Certifications />;
       case 'github':
         return <Github />;
+        case 'notfound':
+            return <NotFound />;
       default:
         return <About />;
     }
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import About from './components/about/About';
-import Resume from './components/resume/Resume';
-import Projects from './components/projects/Projects';
-import Github from './components/github/Github';
 import Root from './components/Root';
 import { Analytics } from "@vercel/analytics/react";
-import Certifications from './components/certification/Certification';
 import NotFound from './components/notfound/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />}>
-      <Route path="" element={<About />} />
-      <Route path="resume" element={<Resume />} />
-      <Route path="projects" element={<Projects />} />
-      <Route path="github" element={<Github />} />
-      <Route path="certification" element={<Certifications />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
