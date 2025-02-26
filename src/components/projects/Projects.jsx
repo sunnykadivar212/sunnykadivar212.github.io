02ac3b5dc1a15602eeb0f2c34013f81d418ce83b
Ashish Vaghela <ashish.vaghela@nelkinda.com>
2025-02-26 16:25:16 +0530

    refactor: Make project view vertical

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -17,7 +17,7 @@ function Projects() {
       <div className="flex flex-col gap-16 lg:gap-6">
         {/* Project 1 */}
         <div className="max-w-screen-lg mx-auto bg-white rounded-lg shadow-md p-6">
-          <div className="flex flex-col items-center gap-4 rounded-2xl py-8 lg:py-12 px-6 lg:px-10 text-slate-900 transition-all duration-700 hover:bg-slate-900 group">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center rounded-2xl py-8 lg:py-12 px-6 lg:px-10 text-slate-900 transition-all duration-700 hover:bg-slate-900 group">
             <img
                 src={data.project1Image || project1Image}
                 alt="project1"
@@ -25,29 +25,31 @@ function Projects() {
                 height={200}
                 className="rounded-lg shadow-lg transition-all duration-300"
             />
-            <div className="text-center text-lg font-semibold mt-4 group-hover:text-white">
-              {data.project1Name}
-            </div>
-            <div className="text-center text-m text-black-700 mt-2 group-hover:text-white">
-              {data.project1Description}
-            </div>
-            <div className="flex gap-4 mt-4">
-              <a
-                  href={data.project1GithubUrl}
-                  className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                GitHub Repo
-              </a>
-              <a
-                  href={data.project1LiveUrl}
-                  className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                Live
-              </a>
+            <div className="text-center md:text-left">
+              <div className="text-lg font-semibold mt-4 group-hover:text-white">
+                {data.project1Name}
+              </div>
+              <div className="text-m text-black-700 mt-2 group-hover:text-white">
+                {data.project1Description}
+              </div>
+              <div className="flex gap-4 mt-4 justify-center md:justify-start">
+                <a
+                    href={data.project1GithubUrl}
+                    className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  GitHub Repo
+                </a>
+                <a
+                    href={data.project1LiveUrl}
+                    className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  Live
+                </a>
+              </div>
             </div>
           </div>
         </div>
